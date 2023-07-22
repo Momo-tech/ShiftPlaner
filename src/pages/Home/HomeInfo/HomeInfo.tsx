fd2bbb00271ababc09e680e5c39fc2d9a33202ab
Matthias Moser <matthiasmoser@freenet.de>
2023-07-22 12:51:39 +0200

    added pause

--- src/pages/Home/HomeInfo/HomeInfo.tsx
+++ src/pages/Home/HomeInfo/HomeInfo.tsx
@@ -14,37 +14,55 @@ export const HomeInfo = (props: HomeInfoProps) => {
   const filteredShits = props.shifts.filter(
     (shift) => dayjs(shift.date).month() === dayjs(month).month()
   );
-  const workHours = calculateWorkHoursForShifts(filteredShits);
+  const pastShifts = filteredShits.filter((shift) => dayjs(shift.date).isBefore(dayjs()));
+  const futureShifts = filteredShits.filter((shift) => dayjs(shift.date).isAfter(dayjs()));
+  const workHoursPast = calculateWorkHoursForShifts(pastShifts);
+  const workHoursFuture = calculateWorkHoursForShifts(futureShifts);
+  const pauseHoursPast = pastShifts.reduce((acc, shift) => {
+    return acc + shift.pause;
+  }, 0);
+  const pauseHoursFuture = futureShifts.reduce((acc, shift) => {
+    return acc + shift.pause;
+  }, 0);
   return (
     <div className="home-info">
       <h2 className="home-info__header">
         Info{" "}
         <MonthPickerInput value={month} onChange={(value) => setMonth(value)} />
       </h2>
-      <div className="home-info-data-box-header">Done</div>
+      <div className="home-info-data-box-header">Vergangene</div>
       <div className="home-info-data-box">
         <div className="home-info-data-box__item">
-          <div>Shifts</div>
+          <div>Schichten</div>
           <div className="home-info-data-box__item--number">
             {filteredShits.length}
           </div>
         </div>
+
         <div className="home-info-data-box__item">
-          <div>Hours</div>
-          <div className="home-info-data-box__item--number">{workHours}</div>
+          <div>Stunden</div>
+          <div className="home-info-data-box__item--number">{workHoursPast}</div>
+        </div>
+        <div className="home-info-data-box__item">
+          <div>Pause</div>
+          <div className="home-info-data-box__item--number">{pauseHoursPast}</div>
         </div>
       </div>
-      <div className="home-info-data-box-header">Planned</div>
+      <div className="home-info-data-box-header">Geplannt</div>
       <div className="home-info-data-box">
         <div className="home-info-data-box__item">
-          <div>Shifts</div>
+          <div>Schichten</div>
           <div className="home-info-data-box__item--number">
             {filteredShits.length}
           </div>
         </div>
         <div className="home-info-data-box__item">
-          <div>Hours</div>
-          <div className="home-info-data-box__item--number">{workHours}</div>
+          <div>Stunden</div>
+          <div className="home-info-data-box__item--number">{workHoursFuture}</div>
+        </div>
+        <div className="home-info-data-box__item">
+          <div>Pause</div>
+          <div className="home-info-data-box__item--number">{pauseHoursFuture}</div>
         </div>
       </div>
     </div>
