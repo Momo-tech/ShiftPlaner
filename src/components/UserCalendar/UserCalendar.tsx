124be780a2eed5b2017e95fb0a639513e828bd1c
Jonas Schmitt <j.schmitt@farmact.de>
2023-11-07 10:27:38 +0100

    feat: ✨ add shift counter for current month

--- src/components/UserCalendar/UserCalendar.tsx
+++ src/components/UserCalendar/UserCalendar.tsx
@@ -1,31 +1,34 @@
-import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin
-import FullCalendar from "@fullcalendar/react";
-import dayjs from "dayjs";
-import { UserShift } from "../../models/UserShift";
-import "./userCalendar.scss";
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin
+import FullCalendar from '@fullcalendar/react';
+import dayjs from 'dayjs';
+import { DatesSetArg } from 'fullcalendar/index.js';
+import { UserShift } from '../../models/UserShift';
+import './userCalendar.scss';
 
 interface UserCalendarPropos {
   userShifts: UserShift[];
+  cb: (dateSetArgs: DatesSetArg) => void;
 }
 
 export function UserCalendar(props: UserCalendarPropos) {
   return (
     <FullCalendar
       headerToolbar={{
-        left: "prev,next today",
-        center: "title",
-        right: "dayGridDay,dayGridWeek,dayGridMonth",
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridDay,dayGridWeek,dayGridMonth'
       }}
       plugins={[dayGridPlugin]}
       locale="de"
       eventDisplay="block"
+      datesSet={props.cb}
       events={[
         ...props.userShifts.map((userShift) => ({
           title: userShift.name,
-          date: dayjs(userShift.date).format("YYYY-MM-DD"),
+          date: dayjs(userShift.date).format('YYYY-MM-DD'),
           start: dayjs(userShift.startTime).toDate(),
-          end: dayjs(userShift.endTime).toDate(),
-        })),
+          end: dayjs(userShift.endTime).toDate()
+        }))
       ]}
       initialView="dayGridMonth"
     />
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { DatesSetArg } from 'fullcalendar/index.js';
 import { UserShift } from 'models/UserShift';
 import { useEffect, useState } from 'react';
 import { getUserShifts } from 'supabase/userShiftFunctions';
@@ -7,6 +8,9 @@ import './home.scss';
 export const Home = () => {
   const user = useUserContext();
   const [userShifts, setUserShifts] = useState<UserShift[]>([]);
+  let [getCount, setCount] = useState<number>(0);
+  let [getDisplay, setDisplay] = useState<boolean>(false);
+
   useEffect(() => {
     handleGetUserShifts();
   }, [user?.id]);
@@ -17,13 +21,81 @@ export const Home = () => {
     }
     const userShifts = await getUserShifts(user.id, user.com_id);
     setUserShifts(userShifts ?? []);
+
+    /* initialize shifts of the current month */
+    let currentMonth = new Date().getMonth();
+    let currentYear = new Date().getFullYear();
+
+    let currentMonthAndYear = `${currentYear}-${currentMonth}`;
+
+    if (userShifts) {
+      const matches = userShifts.filter((shift) => {
+        const shiftMonth = shift.startTime.getMonth();
+
+        const shiftYear = shift.startTime.getFullYear();
+        const shiftMonthAndYear = `${shiftYear}-${shiftMonth}`;
+
+        if (shiftMonthAndYear == currentMonthAndYear) return shift;
+      });
+      setCount(matches.length);
+    }
   };
+
+  const countShiftsForCurrentMonth = (args: DatesSetArg) => {
+    if (args.view.type == 'dayGridWeek') {
+      setCount(0);
+      setDisplay(false);
+      return;
+    }
+    if (args.view.type == 'dayGridDay') {
+      setCount(0);
+      setDisplay(false);
+      return;
+    }
+
+    if (args.startStr == undefined) {
+      setCount(0);
+      return;
+    }
+
+    /* only display shifts when on dayGridMonth view */
+    setDisplay(true);
+
+    let currentYear = +args.startStr.split('-').slice(0, 1);
+    let currentMonth = +args.startStr.split('-').slice(1, 2);
+
+    /* select current month in view */
+    if (currentMonth > 20) {
+      currentMonth += 1;
+      /* december -> january */
+      if (currentMonth > 12) currentMonth = 1;
+    }
+
+    let currentMonthAndYear = `${currentYear}-${currentMonth}`;
+
+    const matches = userShifts.filter((shift) => {
+      const shiftMonth = shift.startTime.getMonth();
+
+      const shiftYear = shift.startTime.getFullYear();
+      const shiftMonthAndYear = `${shiftYear}-${shiftMonth}`;
+
+      if (shiftMonthAndYear == currentMonthAndYear) return shift;
+    });
+
+    setCount(matches.length);
+    return;
+  };
+
   return (
     <div>
       <h2>Übersicht</h2>
       <div className="home-page-grid">
         <div className="home-page-grid__calender">
-          <UserCalendar userShifts={userShifts} />
+          <UserCalendar
+            userShifts={userShifts}
+            cb={countShiftsForCurrentMonth}
+          />
+          {getDisplay ? 'Schichten: ' + getCount : ''}
         </div>
       </div>
     </div>
